a99be5a5023aa797e8f310f4641a8b5573534ff2
Anwesh43 <anweshthecool0@gmail.com>
2020-09-05 23:07:01 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ const sizeFactor : number = 2.9
 const backColor : string = "#bdbdbd"
 const colors : Array<string> = ["#2196F3", "#4CAF50", "#F44336", "#3F51B5", "#FF9800"]
 const delay : number = 90
+const deg : number = Math.PI / 4
+const rot : number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -20,6 +22,42 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRightAngledToSingleLine(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(-deg * sf3)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(-rot * (sf2 - sf4))
+            DrawingUtil.drawLine(context, 0, 0, size * sf1, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawRASLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 2
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRightAngledToSingleLine(context, scale)
     }
 }
 
